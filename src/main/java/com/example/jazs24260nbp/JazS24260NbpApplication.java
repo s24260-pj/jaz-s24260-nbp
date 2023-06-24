b1f4533c43cd1738ccf9a4cab5313c013a4be401
s24260 <s24260@pjwstk.edu.pl>
2023-06-24 14:22:37 +0200

    Dodanie opisu projektu.

--- src/main/java/com/example/jazs24260nbp/JazS24260NbpApplication.java
+++ src/main/java/com/example/jazs24260nbp/JazS24260NbpApplication.java
@@ -1,9 +1,23 @@
 package com.example.jazs24260nbp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "NBP SERVICE REST API",
+				version = "1.0",
+				description = "This is application to save nbp rates.",
+				contact = @Contact(
+						name = "s24260",
+						email = "s24260@pjwstk.edu.pl"
+				)
+		)
+)
 public class JazS24260NbpApplication {
 
 	public static void main(String[] args) {
