4efb0c2e702cf002ea497f607bd548c32eb739f7
s24260 <s24260@pjwstk.edu.pl>
2023-06-24 12:40:21 +0200

    Initial commit

---
+++ src/main/java/com/example/jazs24260nbp/JazS24260NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs24260nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS24260NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS24260NbpApplication.class, args);
+	}
+
+}
